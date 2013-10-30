eed799a26344b02d9ff778675d84a69068ed297a
Fleija <RobinDullens@web.de>
2013-10-30 22:44:51 +0100

    Kleinigkeiten im Menü... XNA ist schrott -.-

--- GameStateManagementSample/Logic/GameMenuRight.cs
+++ GameStateManagementSample/Logic/GameMenuRight.cs
@@ -17,7 +17,7 @@ namespace GameStateManagementSample.Logic
 
         GameStateManagement.ScreenManager screenManager;
         GraphicsDevice graphicsDevice;
-        Texture2D txPixel;
+
 
         public GameMenuRight(int x,int y,int width,int height)
         {
@@ -37,22 +37,32 @@ namespace GameStateManagementSample.Logic
 
         }
 
+        Texture2D txPixel;
+        Rectangle rec;
+
         public void Initialize(GameStateManagement.ScreenManager sm)
         {
             this.screenManager = sm;
             graphicsDevice = sm.GraphicsDevice;
             txPixel = new Texture2D(graphicsDevice, 1, 1, false, SurfaceFormat.Color);
             txPixel.SetData<Color>(new Color[] { Color.White });
+            rec = new Rectangle(x, y, width, height);
         }
 
         public void draw()
         {
-            SpriteBatch spriteBatch = new SpriteBatch(graphicsDevice);
-            Rectangle rec = new Rectangle(x, y, width, height);
+            SpriteBatch spriteBatch = new SpriteBatch(graphicsDevice);            
             spriteBatch.Begin();
           //  LineRenderer.DrawLine(spriteBatch, txPixel, new Vector2(x, y), new Vector2(600, 600), Color.Black, 1f, 1f);
             spriteBatch.Draw(txPixel,rec,Color.Black);
+            spriteBatch.Draw(txPixel,new Rectangle(x+(width/100*10),(y+(height/100*10)),width/100*40,height/100*7),Color.Red);
+            drawTowerPicture(spriteBatch);
             spriteBatch.End();
+        }
+
+        private void drawTowerPicture(SpriteBatch spriteBatch)
+        {
+            //TODO Tower VorschauBilder rendern
         }
     }
 }
