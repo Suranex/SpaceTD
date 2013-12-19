aa6028ac994782dcfedb655f6a345e6c8d5755f4
Rejdukien <rejdukien@googlemail.com>
2013-12-19 02:43:53 +0100

    Assembly Info

--- GameStateManagementSample/Properties/AssemblyInfo.cs
+++ GameStateManagementSample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameStateManagementSample")]
-[assembly: AssemblyProduct("GameStateManagementSample")]
+[assembly: AssemblyTitle("SpaceTD")]
+[assembly: AssemblyProduct("SpaceTD")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
